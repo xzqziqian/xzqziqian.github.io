602de0d0868f273c75f3bd5affe596209a07a882
Ziqian Xu <38840988+xzqziqian@users.noreply.github.com>
2025-05-08 10:41:26 +0800

    remove extra

--- themes/tranquility/scripts/filters/stylus.js
+++
@@ -1,7 +0,0 @@
-module.exports = hexo.extend.filter.register('stylus:renderer', function (style) {
-  const url_for = hexo.extend.helper.get('url_for').bind(hexo);
-  style
-    .define('url_for', function (data) {
-      return url_for(data.val);
-    });
-});
